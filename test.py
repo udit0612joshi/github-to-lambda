5c93337a12c6f2ebfd5af871adb5d92f568bed6f
udit0612joshi <uditjoshi0612@gmail.com>
2021-07-09 15:25:23 +0530

    adding lamda function

---
+++ test.py
@@ -0,0 +1,38 @@
+import json
+import boto3
+from datetime import datetime
+
+from botocore.exceptions import ClientError
+
+BUCKET_NAME = 'testbucket345346'
+
+
+def respond(res):
+    return {
+        'statusCode': '200',
+        'body': json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
+    body = ""
+    try:
+        obj = s3.Object(BUCKET_NAME, "udit.txt")
+        body = obj.get()['Body'].read().decode()
+        print(body)
+    except ClientError:
+        print("Key Not found")
+
+    client = boto3.client('s3')
+    date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+    client.put_object(Body=str(body) + "\n"+ date, Bucket=BUCKET_NAME, Key='udit.txt')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('helloworld')
+    }
+
