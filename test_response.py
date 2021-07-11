2ee6a121c1fd775ec3dd2eb4684ebbd798236591
udit0612joshi <34023168+udit0612joshi@users.noreply.github.com>
2021-07-11 13:26:12 +0530

    Update test_response.py

--- test_response.py
+++ test_response.py
@@ -1,5 +1,5 @@
 import requests
-import lambda_function
+from lambda_function import *
 
 def test_api():
     res = requests.get('https://maas04yks8.execute-api.us-east-2.amazonaws.com/default/helloWorld')
