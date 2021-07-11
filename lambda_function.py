7aae5e55159365cd8ad11cc37f278aca8f57eb5c
udit0612joshi <34023168+udit0612joshi@users.noreply.github.com>
2021-07-11 12:32:19 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,9 +2,12 @@ import json
 import boto3
 from datetime import datetime
 
+from boto3 import s3
 from botocore.exceptions import ClientError
 
 BUCKET_NAME = 'testbucket345346'
+JSON_STRING = 'helloworld'
+FILE_NAME = 'data.csv'
 
 
 def respond(res):
@@ -17,21 +20,22 @@ def respond(res):
     }
 
 
-def lambda_handler(event, context):
+def s3_read_write():
     s3 = boto3.resource('s3')
     body = ""
     try:
-        obj = s3.Object(BUCKET_NAME, "udit.txt")
+        obj = s3.Object(BUCKET_NAME, FILE_NAME)
         body = obj.get()['Body'].read().decode()
-        print("Changing comments")
     except ClientError:
         print("Key Not found")
-
     client = boto3.client('s3')
     date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
-    client.put_object(Body=str(body) + "\n"+ date, Bucket=BUCKET_NAME, Key='udit.txt')
+    client.put_object(Body=str(body) + "\n" + date, Bucket=BUCKET_NAME, Key=FILE_NAME)
 
+
+def lambda_handler(event, context):
+    s3_read_write()
     return {
         'statusCode': 200,
-        'body': json.dumps('helloworld')
+        'body': json.dumps(JSON_STRING)
     }
