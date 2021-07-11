da3642dd772a50acf35483b12140f3069c839aa5
udit0612joshi <34023168+udit0612joshi@users.noreply.github.com>
2021-07-11 13:14:20 +0530

    Update test_response.py

--- test_response.py
+++ test_response.py
@@ -3,8 +3,7 @@ import lambda_function
 
 def test_api():
     res = requests.get('https://maas04yks8.execute-api.us-east-2.amazonaws.com/default/helloWorld'
-    assert res.status_code == 200
-    assert JSON_STRING=="helloWorld", "Output is not valid"
+    assert JSON_STRING == "helloWorld", "Output is not valid"
     assert res.json() == "helloworld",  "Output is not valid"
 
 
