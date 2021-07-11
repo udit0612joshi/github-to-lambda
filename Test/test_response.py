6c1873283589ff86e5546dc6a8bb8edf2d63f3e2
udit0612joshi <34023168+udit0612joshi@users.noreply.github.com>
2021-07-11 12:41:14 +0530

    Delete Test directory

--- Test/test_response.py
+++
@@ -1,9 +0,0 @@
-import requests
-import lambda_function.lambda_handler
-
-
-def test_api():
-    res = requests.get('https://maas04yks8.execute-api.us-east-2.amazonaws.com/default/helloWorld')
-    assert res.status_code == 200
-    assert JSON_STRING == "helloWorld", "Output is not valid"
-    assert res.json() == "helloworld", "Output is not valid"
