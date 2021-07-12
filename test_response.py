309c56f1f6b2ab3b0d9b55c16702a10915092907
udit0612joshi <34023168+udit0612joshi@users.noreply.github.com>
2021-07-12 14:20:16 +0530

    Update test_response.py

--- test_response.py
+++ test_response.py
@@ -2,6 +2,4 @@ import requests
 from lambda_function import *
 
 def test_api():
-    res = requests.get('https://maas04yks8.execute-api.us-east-2.amazonaws.com/default/helloWorld')
-    assert JSON_STRING == "helloworld", "Output is not valid"
-    assert res.json() == "helloworld", "Output is not valid"
+    hello_world() == "helloworld" , "Output is not valid"
