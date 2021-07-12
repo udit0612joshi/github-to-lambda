745b28b56b8890e6454e47ef23a888b1b749aa9f
udit0612joshi <34023168+udit0612joshi@users.noreply.github.com>
2021-07-12 12:10:12 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -6,7 +6,7 @@ from boto3 import s3
 from botocore.exceptions import ClientError
 
 BUCKET_NAME = 'testbucket345346'
-JSON_STRING = 'helloworld'
+JSON_STRING = 'byteworld'
 FILE_NAME = 'data.csv'
 
 
